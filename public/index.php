058f94eddc48313d493e3a8a3457f3c7b47a709d
Christian McDonald <cmcdonald@statesman.com>
2015-07-01 16:34:01 -0500

    base setup

---
+++ public/index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <?php
+  $meta = array(
+    "title" => "2015 Statesman Social Media Awards | Statesman.com",
+    "description" => "The Austin American-Statesman is honoring the best in social media in Central Texas. Use our online nomination form to nominate your favorite person, company or group for a Statesman Social Media Award.",
+    "thumbnail" => "http://projects.statesman.com/then-and-now/paramount/assets/share.png", // needs update
+    "shortcut_icon" => "http://media.cmgdigital.com/shared/theme-assets/242014/www.statesman.com_5126cb2068bd43d1ab4e17660ac48255.ico",
+    "apple_touch_icon" => "http://media.cmgdigital.com/shared/theme-assets/242014/www.statesman.com_fa2d2d6e73614535b997734c7e7d2287.png",
+    "url" => "http://projects.statesman.com/ssma/",
+    "twitter" => "statesman"
+  );
+?>
+
+  <title>Interactive: <?php print $meta['title']; ?> | Austin American-Statesman</title>
+  <link rel="shortcut icon" href="<?php print $meta['shortcut_icon']; ?>" />
+  <link rel="apple-touch-icon" href="<?php print $meta['apple_touch_icon']; ?>" />
+
+  <link rel="canonical" href="<?php print $meta['url']; ?>" />
+
+  <meta name="description" content="<?php print $meta['description']; ?>">
+
+  <meta property="og:title" content="<?php print $meta['title']; ?>"/>
+  <meta property="og:description" content="<?php print $meta['description']; ?>"/>
+  <meta property="og:image" content="<?php print $meta['thumbnail']; ?>"/>
+  <meta property="og:url" content="<?php print $meta['url']; ?>"/>
+
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@<?php print $meta['twitter']; ?>" />
+  <meta name="twitter:title" content="<?php print $meta['title']; ?>" />
+  <meta name="twitter:description" content="<?php print $meta['description']; ?>" />
+  <meta name="twitter:image" content="<?php print $meta['thumbnail']; ?>" />
+  <meta name="twitter:url" content="<?php print $meta['url']; ?>" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="dist/style.css">
+
+  <link href='http://fonts.googleapis.com/css?family=Lusitana:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:400,300,300italic,400italic,700italic,700,800,800italic' rel='stylesheet' type='text/css'>
+ 
+
+  <?php /* CMG advertising and analytics */ ?>
+  <?php include "includes/advertising.inc"; ?>
+  <?php include "includes/metrics-head.inc"; ?>
+
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+
+        <a class="navbar-brand" href="http://www.statesman.com/" target="_blank">
+        <img class="visible-xs visible-sm" width="103" height="26" src="assets/logo-short-black.png" />
+        <img class="hidden-xs hidden-sm" width="273" height="26" src="assets/logo.png" />
+        </a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="./">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="visible-xs small-social"><a target="_blank" href="https://www.facebook.com/sharer.php?u=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-facebook-square"></i></a><a target="_blank" href="https://twitter.com/intent/tweet?url=<?php echo urlencode($meta['url']); ?>&via=<?php print urlencode($meta['twitter']); ?>&text=<?php print urlencode($meta['title']); ?>"><i class="fa fa-twitter"></i></a><a target="_blank" href="https://plus.google.com/share?url=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-google-plus"></i></a></li>
+      </ul>
+        <ul class="nav navbar-nav navbar-right social hidden-xs">
+          <li><a target="_blank" href="https://www.facebook.com/sharer.php?u=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-facebook-square"></i></a></li>
+          <li><a target="_blank" href="https://twitter.com/intent/tweet?url=<?php echo urlencode($meta['url']); ?>&via=<?php print urlencode($meta['twitter']); ?>&text=<?php print urlencode($meta['title']); ?>"><i class="fa fa-twitter"></i></a></li>
+          <li><a target="_blank" href="https://plus.google.com/share?url=<?php echo urlencode($meta['url']); ?>"><i class="fa fa-google-plus"></i></a></li>
+        </ul>
+    </div>
+  </div>
+</nav>
+<div id="back">
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 header">
+      <h1 id="pagetitle">Meet the area's 2015 top high school athletes</h1>
+
+      <p>The final buzzer has sounded for this year's Austin-area high school sports, and you know what that means. Every year the Statesman's sports staff compiles a list of top athletes, a who's-who in the region's high school sports.</p>
+      <p>Let's relive the season and see who made the cut:</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4">
+        <h3>Top 10 high school athletes</h3>
+        <p>Ranging from 10 different area schools, these athletes are the best in the region. Here's how the Top 10 broke down:</p>
+        <ul>
+          <li>Schools represented: 10</li>
+          <li>Number of seniors: 8</li>
+          <li>Number of juniors: 1</li>
+          <li>Number of sophomores: 1</li>
+          <li>Sports represented: Baseball, basketball, football, softball, track & field, volleyball</li>
+        </ul>
+      </div>
+      <div class="col-lg-8">
+        <div style='display: inline-block; width: 100%;'><div style='position: relative; padding-bottom: 100%; padding-top:25px; height: 0;'><iframe src="//top-50-high-school-athletes.silk.co/s/embed/grid/collection/top-10-high-school-athletes/column/school/column/year/image/image/description/accomplishment/slice/0/10" style="border:0;position: absolute; top:0; left:0; width: 100%;height:100%;"></iframe></div><div style='margin-top:10px;font-size:12px;color:gray;'>Data from <a href='http://top-50-high-school-athletes.silk.co'>top-50-high-school-athletes.silk.co</a></div></div>
+      </div>
+    </div>
+
+
+
+
+  </div>
+
+    <!-- bottom matter -->
+    <?php include "includes/banner-ad.inc";?>
+    <?php include "includes/legal.inc";?>
+    <?php include "includes/project-metrics.inc"; ?>
+    <?php include "includes/metrics.inc"; ?>
+
+    <script src="dist/scripts.js"></script>
+
+</div>
+
+  <?php if($_SERVER['SERVER_NAME'] === 'localhost'): ?>
+    <script src="//localhost:35729/livereload.js"></script>
+  <?php endif; ?>
+</body>
+</html>
